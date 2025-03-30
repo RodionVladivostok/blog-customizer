831b8fbc15bcc9b91319ed0bd8ca8580fb842cfc
Rodion Voronin <rdvoronin@gmail.com>
2025-03-31 02:20:52 +1000

    ПР-10 на первичную проверку

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,143 @@
+import {
+	FunctionComponent,
+	ReactElement,
+	SyntheticEvent,
+	useRef,
+	useState,
+} from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (articleState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm: FunctionComponent<ArticleParamsFormProps> = ({
+	articleState,
+	setArticleState,
+}): ReactElement<ArticleParamsFormProps> => {
+	const [isOpen, setIsOpen] = useState<boolean>(false),
+		[currentArticleState, setCurrentArticleState] =
+			useState<ArticleStateType>(articleState),
+		rootRef = useRef<HTMLDivElement>(null);
+
+	const handleSelectedOptionChange = (
+		key: keyof ArticleStateType,
+		option: OptionType
+	) => {
+		setCurrentArticleState({
+			...currentArticleState,
+			[key]: option,
+		});
+	};
+
+	const handleFormSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		setArticleState(currentArticleState);
+	};
+
+	const handleFormReset = () => {
+		setCurrentArticleState(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onChange: setIsOpen,
+	});
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx([
+					styles.container,
+					{ [styles.container_open]: isOpen },
+				])}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
+					<Text size={31} weight={800} uppercase>
+						задайте параметры
+					</Text>
+					<div className={styles.spacing} />
+					<Select
+						title='шрифт'
+						selected={currentArticleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							handleSelectedOptionChange('fontFamilyOption', option)
+						}
+					/>
+					<div className={styles.spacing} />
+					<RadioGroup
+						name='fontSizeOptions'
+						options={fontSizeOptions}
+						selected={currentArticleState.fontSizeOption}
+						title='размер шрифта'
+						onChange={(option) =>
+							handleSelectedOptionChange('fontSizeOption', option)
+						}
+					/>
+					<div className={styles.spacing} />
+					<Select
+						title='цвет шрифта'
+						selected={currentArticleState.fontColor}
+						options={fontColors}
+						onChange={(option) =>
+							handleSelectedOptionChange('fontColor', option)
+						}
+					/>
+					<div className={styles.spacing} />
+					<Separator />
+					<div className={styles.spacing} />
+					<Select
+						title='цвет фона'
+						selected={currentArticleState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) =>
+							handleSelectedOptionChange('backgroundColor', option)
+						}
+					/>
+					<div className={styles.spacing} />
+					<Select
+						title='ширина контента'
+						selected={currentArticleState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) =>
+							handleSelectedOptionChange('contentWidth', option)
+						}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							onClick={handleFormReset}
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
